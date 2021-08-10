8189ea431f6201f5004028898db7341e0fb5da82
bigwizzo <wnnwillard@gmail.com>
2021-08-10 20:34:40 +0200

    Initialize sessions controller create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :students
+  # devise_for :students
   namespace :v1 do
     resources :subjects
     resources :clockings
+    resources :sessions, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
