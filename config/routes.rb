46c9158bb45c13a21737a2891ec4ee49f6b1cf4c
bigwizzo <wnnwillard@gmail.com>
2021-08-10 19:09:36 +0200

    Add subject route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :subjects
+  namespace :v1 do
+    resources :subjects
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
