99684b23bfc71a6aa3113fb3094f4e9bcb50527d
bigwizzo <wnnwillard@gmail.com>
2021-08-11 20:20:26 +0200

    Render token with student_id and username

--- app/controllers/v1/authentication_controller.rb
+++ app/controllers/v1/authentication_controller.rb
@@ -7,8 +7,11 @@ class V1::AuthenticationController < ApplicationController
     else
       if student.authenticate(params[:password])
         secret_key = Rails.application.secrets.secret_key_base[0]
-        token = JWT.encode(student, secret_key)
-        render json: token
+        token = JWT.encode({
+          student_id: student.id,
+          username: student.username,
+        }, secret_key)
+        render json: {token: token}
       else
         render json: {message: "wrong password"}
       end
