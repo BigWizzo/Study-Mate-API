79d94ef18535bbf11e9b62d34050b36a96f28fed
bigwizzo <wnnwillard@gmail.com>
2021-08-11 17:39:30 +0200

    Add student model and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
+    resources :students, only: [:create]
     resources :subjects
     resources :clockings
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_171043) do
+ActiveRecord::Schema.define(version: 2021_08_11_153424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_08_10_171043) do
     t.string "topic"
     t.string "details"
     t.integer "duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
