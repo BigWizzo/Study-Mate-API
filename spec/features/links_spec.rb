576968cf5434ca7b91e7988385e81046630c77b0
Will Nyamunokora <wnnwillard@gmail.com>
2021-09-20 23:28:38 +0200

    Add student model specs

---
+++ spec/features/links_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Test navigation links' do
+  before :each do
+    Student.create(username: 'myname', password: 'mypassword')
+  end
+
+  it 'Send student to the all subjects page' do
+    visit v1_subjects_path
+    expect(current_path).to eql(v1_subjects_path)
+  end
+
+  it 'Should not send student to the clockings page' do
+  visit v1_subjects_path
+  expect(current_path).not_to eql(v1_clockings_path)
+end
+
+end
