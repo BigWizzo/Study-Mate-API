917f06a1b5fcfec50be6dbc766c35842cd1ff7be
Will Nyamunokora <wnnwillard@gmail.com>
2021-09-21 03:11:39 +0200

    Fix secret_key_base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
     authorization_header = request.headers[:authorization]
     if authorization_header
       token = authorization_header.split[1]
-      secret_key = Rails.application.secrets.secret_key_base[0]
+      secret_key = Rails.application.secret_key_base[0]
       decode_token = JWT.decode(token, secret_key)
       @student = Student.find(decode_token[0]['student_id'])
     else
